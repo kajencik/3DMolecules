1982d0ebe177f06711209f45d51b897956c0448e
Kajen <kajen@example.com>
2025-10-24 18:09:56 +0200

    Add comprehensive README and fluid simulation features for v2.1

--- 3DMolecules/obj/Debug/net8.0-windows/3DMolecules.AssemblyInfo.cs
+++ 3DMolecules/obj/Debug/net8.0-windows/3DMolecules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3DMolecules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b0679bd589d95de5eea1ca04d4b30ad2b7a9b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c7ad0ceba2212adc7db057b0c3fc715dd17909")]
 [assembly: System.Reflection.AssemblyProductAttribute("3DMolecules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3DMolecules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ commit-project.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo Committing 3DMolecules project...
+git add .
+git commit -m "Add comprehensive README and fluid simulation features for v2.1"
+git push origin master
+echo Done!
+pause
